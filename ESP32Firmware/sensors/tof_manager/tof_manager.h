5c3cc72d6e1fd27fcc784dc041be5f5fcb5976bc
Tharun Sha <sg9242182@gmail.com>
2026-09-05 11:53:43 +0530

    Expose ToF range status for validation

--- ESP32Firmware/sensors/tof_manager/tof_manager.h
+++ ESP32Firmware/sensors/tof_manager/tof_manager.h
@@ -19,6 +19,7 @@ public:
     float getDistanceCm(SensorId sensor) const;
     bool isValid(SensorId sensor) const;
     bool isObstacleDetected(SensorId sensor) const;
+    uint8_t getRangeStatus(SensorId sensor) const;
 
 private:
     static constexpr uint8_t SDA_PIN = 21;
@@ -29,6 +30,7 @@ private:
 
     Adafruit_VL53L0X sensors[SENSOR_COUNT];
     uint16_t distanceMm[SENSOR_COUNT] = {0, 0, 0};
+    uint8_t rangeStatus[SENSOR_COUNT] = {255, 255, 255};
     bool valid[SENSOR_COUNT] = {false, false, false};
     bool obstacleDetected[SENSOR_COUNT] = {false, false, false};
     unsigned long lastMeasurementTime = 0;
